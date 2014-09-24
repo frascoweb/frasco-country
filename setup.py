4095cd273c018fb913958953953ec8552a9dc88a
maximebf <maxime.bouroumeau@gmail.com>
2014-09-24 10:26:05 +0200

    added requirements.txt

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ def desc():
     with open("README.md") as f:
         return f.read()
 
+def reqs():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
 
 setup(
     name='frasco-assets',
@@ -17,8 +20,5 @@ setup(
     long_description=desc(),
     packages=find_packages(),
     platforms='any',
-    install_requires=[
-        'frasco',
-        'pycountry==1.8'
-    ]
+    install_requires=reqs() + ['frasco']
 )
